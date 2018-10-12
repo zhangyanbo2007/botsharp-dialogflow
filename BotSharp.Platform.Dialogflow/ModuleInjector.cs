7b52da3fbb0d58ed8e6b3e0353beac8687917a86
Oceania2018 <haiping008@gmail.com>
2018-10-11 21:43:54 -0500

    optimize agent storage DI.

--- BotSharp.Platform.Dialogflow/ModuleInjector.cs
+++ BotSharp.Platform.Dialogflow/ModuleInjector.cs
@@ -1,4 +1,5 @@
 ﻿using BotSharp.Core;
+using BotSharp.Core.AgentStorage;
 using BotSharp.Core.Modules;
 using BotSharp.Platform.Abstraction;
 using BotSharp.Platform.Dialogflow.Models;
@@ -12,36 +13,11 @@ namespace BotSharp.Platform.Dialogflow
 {
     public class ModuleInjector : IModule
     {
-        public void ConfigureServices(IServiceCollection services,IConfiguration config)
+        public void ConfigureServices(IServiceCollection services, IConfiguration config)
         {
-            services.TryAddSingleton<IPlatformSettings, PlatformSettings>();
-            services.TryAddSingleton<IAgentStorageFactory<AgentModel>, AgentStorageFactory<AgentModel>>();
-            services.TryAddSingleton<DialogflowAi<AgentModel>>();
-
-            var setting = new PlatformSettings();
-            config.GetSection("DialogflowAi").Bind(setting);
-            services.AddSingleton(setting);
-            services.AddSingleton<AgentStorageInMemory<AgentModel>>();
-            services.AddSingleton<AgentStorageInRedis<AgentModel>>();
-            services.AddSingleton(factory =>
-            {
-                Func<string, IAgentStorage<AgentModel>> accesor = key =>
-                {
-                    if (key.Equals("AgentStorageInRedis"))
-                    {
-                        return factory.GetService<AgentStorageInRedis<AgentModel>>();
-                    }
-                    else if (key.Equals("AgentStorageInMemory"))
-                    {
-                        return factory.GetService<AgentStorageInMemory<AgentModel>>();
-                    }
-                    else
-                    {
-                        throw new ArgumentException($"Not Support key : {key}");
-                    }
-                };
-                return accesor;
-            });
+            services.AddSingleton<DialogflowAi<AgentModel>>();
+            AgentStorageServiceRegister.Register<AgentModel>(services);
+            PlatformConfigServiceRegister.Register<PlatformSettings>("dialogflowAi", services, config);
         }
 
         public void Configure(IApplicationBuilder app)
